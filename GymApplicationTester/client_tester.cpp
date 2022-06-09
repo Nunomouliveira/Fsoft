c3fc30ebf41c6c673f5433b0ca03a6b085efdcc6
1211593 <1211593@isep.ipp.pt>
2022-06-09 16:07:36 +0100

    Criação do ficheiro client_tester

---
+++ GymApplicationTester/client_tester.cpp
@@ -0,0 +1,64 @@
+//
+// Created by migue on 09/06/2022.
+//
+
+# include "gtest/gtest.h"
+# include "client.h"
+# include "InvalidDataException.h"
+
+TEST(ClientConstructorTest, InvalidName) {
+    bool flag = false;
+    try{
+        Client client ("a", "18");
+    }catch(InvalidDataException& e){
+        flag = true;
+    }
+    EXPECT_TRUE(flag);
+}
+
+TEST(ClientConstructorTest, ValidName) {
+    bool flag = false;
+    try{
+        Client client ("Miguel", "18");
+    }catch(InvalidDataException& e){
+        flag = true;
+    }
+    EXPECT_FALSE(flag);
+}
+
+TEST(ClientSetNameTest, InvalidName) {
+    Client client ("Miguel", "18");
+    bool flag = false;
+    try{
+        client.setName("a");
+    }catch(InvalidDataException& e){
+        flag = true;
+    }
+    EXPECT_TRUE(flag);
+}
+
+TEST(ClientSetNameTest, ValidName) {
+    Client client ("Miguel", "18");
+    bool flag = false;
+    try{
+        Client client ("Nuno", "18");
+    }catch(InvalidDataException& e){
+        flag = true;
+    }
+    EXPECT_FALSE(flag);
+}
+TEST(ClientOperatorEqualTest, EqualNrs) {
+    Client client ("Miguel", "18");
+    int nr = client.getNumber();
+    bool flag = client==nr;
+    EXPECT_TRUE(flag);
+}
+TEST(ClientOperatorEqualTest, NotEqualNrs) {
+    Client client ("Miguel", "18");
+    int nr = client.getNumber() +1;
+    bool flag = client==nr;
+    EXPECT_FALSE(flag);
+}
+
+
+
