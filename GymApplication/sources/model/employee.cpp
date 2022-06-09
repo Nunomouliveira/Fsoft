5d2eb3508003f024385046399d630274f2fa1059
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:38:40 +0100

    Criação do ficheiro employee.cpp

---
+++ GymApplication/sources/model/employee.cpp
@@ -0,0 +1,62 @@
+//
+// Created by migue on 09/06/2022.
+//
+#include "employee.h"
+#include "InvalidDataException.h"
+
+bool Employee::ValidInitials(const string &initials) {
+    if(initials.length() != 4){
+        return false;
+    }
+    return true;
+}
+
+Employee::Employee(const string &initials, const string &name) :Person(name){
+    setInitials(initials);
+}
+
+Employee::Employee(const Employee &obj) : Person(obj.name){
+    setInitials(obj.initials);
+    this->lectures = obj.lectures;
+}
+
+Employee::~Employee(){
+
+}
+
+const string& Employee::getInitials() const {
+    return initials;
+}
+
+void Employee::setInitials(const string &initials) {
+
+    if(ValidInitials(initials)){
+        this->initials = initials;
+    }else{
+
+        string msg = "Employee: " + initials;
+        throw InvalidDataException(msg);
+    }
+}
+LectureContainer & Employee::getLectures(){
+    return this->lectures;
+}
+
+bool Employee::operator == (const Employee& obj) const{
+    if(this->initials == obj.initials){
+        return true;
+    }
+    return false;
+}
+
+bool Employee::operator == (const string initials) const{
+    if(this->initials == initials){
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
