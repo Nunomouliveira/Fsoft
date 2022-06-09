84df6f62b094a3bb6949928bef1091d45b7adfda
Nuno Oliveira <1210939@isep.ipp.pt>
2022-06-09 14:42:25 +0100

    criar ficheiro view.h

---
+++ GymApplication/headers/view/view.h
@@ -0,0 +1,24 @@
+//
+// Created by nunon on 09/06/2022.
+//
+
+#ifndef FSOFT2022_1A_4_VIEW_H
+#define FSOFT2022_1A_4_VIEW_H
+
+#include <string>
+
+using namespace std;
+class View{
+public:
+    View();
+    int menuGym();
+    int menuClients();
+    int menuEmployees();
+    int menuClasses();
+    int menuEnrolls();
+    int menuLectures();
+
+
+};
+
+#endif //FSOFT2022_1A_4_VIEW_H
