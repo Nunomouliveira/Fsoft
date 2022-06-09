8cc6186a17fa0bcf3542b611093bbcaa2f7bdc24
Raul1211524 <1211524@isep.ipp.pt>
2022-06-09 16:31:59 +0100

    Ficheiro controllers.h

---
+++ GymApplication/headers/controllers/controllers.h
@@ -0,0 +1,36 @@
+//
+// Created by raulr on 09/06/2022.
+//
+
+#ifndef GYMAPPLICATION_CONTROLLERS_H
+#define GYMAPPLICATION_CONTROLLERS_H
+
+#include "employeeView.h"
+#include "clientView.h"
+#include "view.h"
+#include "enrollView.h"
+#include "lectureView.h"
+#include "gym.h"
+#include "feedbackView.h"
+
+class Controller{
+private:
+    gym model;
+    ClientView clientView;
+    EmployeeView employeeView;
+    ClassView classView;
+    EnrollView enrollView;
+    LectureView lectureView;
+    View view;
+    FeedbackView feedbackView;
+    void runClients();
+    void runEmployees();
+    void runClasses();
+    void runEnrolls();
+    void runLectures();
+public:
+    Controller(gym& gym);
+    void run();
+};
+
+#endif //GYMAPPLICATION_CONTROLLERS_H
