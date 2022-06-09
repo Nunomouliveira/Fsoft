88df7ff75d06234b95f17fa1772494e24e34a8a9
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:33:44 +0100

    Criação do ficheiro employeeContainer.h

---
+++ GymApplication/headers/model/employeeContainer.h
@@ -0,0 +1,22 @@
+//
+// Created by migue on 09/06/2022.
+//
+
+#ifndef GYMAPPLICATION_EMPLOYEECONTAINER_H
+#define GYMAPPLICATION_EMPLOYEECONTAINER_H
+#include <list>
+#include "employee.h"
+
+class EmployeeContainer{
+private:
+    list<Employee> employees;
+public:
+    list<Employee>::iterator search(const string& initials);
+    list<Employee> getAll();
+    Employee* get(const string& initials);
+    void add(const Employee& obj);
+    void remove(const string& initials);
+    bool anyClass(const string& initials);
+};
+
+#endif //GYMAPPLICATION_EMPLOYEECONTAINER_H
