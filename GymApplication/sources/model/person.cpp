72eb5b7c76a128543c9250a5cadc7e2134430005
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:41:48 +0100

    Criação do ficheiro person.cpp

---
+++ GymApplication/sources/model/person.cpp
@@ -0,0 +1,39 @@
+//
+// Created by migue on 09/06/2022.
+//
+#include "person.h"
+#include "InvalidDataException.h"
+
+
+
+bool Person::ValidName(const string &name) {
+    if(name.length() < 3){
+        return false;
+    }
+    return true;
+}
+
+Person::Person(const string& name){
+    setName(name);
+}
+Person::Person(const Person& person){
+    setName(person.name);
+}
+
+Person::~Person(){
+}
+
+const string&  Person::getName() const {
+    return name;
+}
+void  Person::setName(const string &name) {
+    if(ValidName(name)){
+        this->name = name;
+    }else{
+
+        string msg = "Person: " + name;
+        throw InvalidDataException(msg);
+    }
+}
+
+
