fd07284ded4855cc8ad4ceb95ca9bbc3da7d0abc
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:34:45 +0100

    Criação do ficheiro lecture.h

---
+++ GymApplication/headers/model/lecture.h
@@ -0,0 +1,22 @@
+//
+// Created by migue on 09/06/2022.
+//
+
+#ifndef GYMAPPLICATION_LECTURE_H
+#define GYMAPPLICATION_LECTURE_H
+#include "class.h"
+class Lecture{
+private:
+    Class * classes;
+public:
+    Lecture(Class * classes);
+    Lecture(const Lecture& obj);
+    ~Lecture();
+    Class* getClass() const;
+    void setClass(Class *subject);
+    bool operator == (const Lecture& obj) const;
+    bool operator == (const Class* ptr) const;
+};
+
+
+#endif //GYMAPPLICATION_LECTURE_H
