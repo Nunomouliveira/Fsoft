9ba29ecd9652535348382c2c7713ba5a7118d134
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:32:56 +0100

    Criação do ficheiro client.h

---
+++ GymApplication/headers/model/client.h
@@ -0,0 +1,31 @@
+//
+// Created by migue on 09/06/2022.
+//
+
+#ifndef GYMAPPLICATION_CLIENT_H
+#define GYMAPPLICATION_CLIENT_H
+#include "person.h"
+
+class Client : public Person{
+private:
+    static int NUMBER;
+    int number;
+    string age;
+
+
+public:
+    Client(const string& name, const string& age);
+    Client(const Client& client);
+    ~Client();
+    int getNumber() const ;
+    void setNumber(int number);
+    string getAge() const;
+    void setAge(string age);
+    bool operator == (const Client& obj) const;
+    bool operator == (int nr) const;
+
+
+};
+
+
+#endif //GYMAPPLICATION_CLIENT_H
