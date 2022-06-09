0e978ddc7df907f2389f6613c15bacf07bacd532
1211593 <1211593@isep.ipp.pt>
2022-06-09 16:49:45 +0100

    Ficheiro main.cpp

--- GymApplication/main.cpp
+++ GymApplication/main.cpp
@@ -1,4 +1,17 @@
 //
 // Created by migue on 09/06/2022.
 //
+#include "gym.h"
+#include "mockdata.h"
+#include "controllers.h"
+using namespace std;
+
+int main() {
+    Gym gym("Ginásio");
+    mockdata mock;
+    mock.gData(gym);
+    Controller controller(gym);
+    controller.run();
+    return 0;
+}
 
