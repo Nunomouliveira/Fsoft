8d5cfc5d28961c812d7cf6c8c6ce604fdfaf5831
Nuno Oliveira <1210939@isep.ipp.pt>
2022-06-09 14:42:08 +0100

    criar ficheiro utils.h

---
+++ GymApplication/headers/view/utils.h
@@ -0,0 +1,18 @@
+//
+// Created by nunon on 09/06/2022.
+//
+
+#ifndef FSOFT2022_1A_4_UTILS_H
+#define FSOFT2022_1A_4_UTILS_H
+
+#include <string>
+using namespace std;
+class Utils{
+
+public:
+    static int getNumber(const string&  label);
+    static string getString(const string&  label);
+};
+
+#endif //FSOFT2022_1A_4_UTILS_H
+
