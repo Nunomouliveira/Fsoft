5a61751ed3a06d24a13861545a264739225ac265
Nuno Oliveira <1210939@isep.ipp.pt>
2022-06-09 14:41:29 +0100

    criar ficheiro employeeView.h

---
+++ GymApplication/headers/view/employeeView.h
@@ -0,0 +1,16 @@
+//
+// Created by nunon on 09/06/2022.
+//
+
+#ifndef FSOFT2022_1A_4_EMPLOYEEVIEW_H
+#define FSOFT2022_1A_4_EMPLOYEEVIEW_H
+
+#include <list>
+#include "../model/employee.h"
+class EmployeeView{
+public:
+    Employee getEmployee();
+    void printEmployee(Employee *employee);
+    void printEmployees(list<Employee>& employees);
+
+#endif //FSOFT2022_1A_4_EMPLOYEEVIEW_H
