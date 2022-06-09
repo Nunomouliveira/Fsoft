c7c0f221daa585742fc4a4a920b2624acc44654e
1211593 <1211593@isep.ipp.pt>
2022-06-09 12:41:31 +0100

    Criação do ficheiro lectureContainer.cpp

---
+++ GymApplication/sources/model/lectureContainer.cpp
@@ -0,0 +1,48 @@
+//
+// Created by migue on 09/06/2022.
+//
+#include "DuplicatedDataException.h"
+#include "lectureContainer.h"
+
+list<Lecture>::iterator LectureContainer::search(const string& initials){
+    Class * classes;
+    list<Lecture>::iterator it = this->lectures.begin();
+    for (; it != this->lectures.end(); ++it){
+        classes = it->getClass();
+        if(*(classes) == initials){
+            return it;
+        }
+    }
+    return it;
+}
+list<Lecture>  LectureContainer::getAll(){
+    list<Lecture> newlist (this->lectures);
+    return newlist;
+}
+Lecture*  LectureContainer::get(const string& initials){
+    list<Lecture>::iterator it = search(initials);
+    if(it != this->lectures.end()){
+        return &*it;
+    }
+    return NULL;
+}
+void  LectureContainer::add(const Lecture& obj){
+    Class* subject = obj.getClass();
+    list<Lecture>::iterator it = search(subject->getInitials());
+    if(it == this->lectures.end()){
+        this->lectures.push_back(obj);
+    }else{
+        string msg = "Lecture: " + subject->getInitials();
+        throw  DuplicatedDataException(msg);
+    }
+
+}
+void  LectureContainer::remove(const string& initials){
+    list<Lecture>::iterator it = search(initials);
+    if(it != this->lectures.end()){
+        this->lectures.erase(it);
+    }
+}
+
+
+
