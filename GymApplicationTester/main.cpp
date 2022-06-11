83b3ed04c430f41c331afb093db38d69e4ffc3b6
1211593 <1211593@isep.ipp.pt>
2022-06-11 11:55:29 +0100

    Ficheiro main.cpp tester

--- GymApplicationTester/main.cpp
+++ GymApplicationTester/main.cpp
@@ -2,14 +2,22 @@
 // Created by migue on 09/06/2022.
 //
 
-#include <iostream>
+
 #include <gtest/gtest.h>
 #include "client_tester.cpp"
 #include "clientContainer_tester.cpp"
 #include "employee_tester.cpp"
 #include "employeeContainer_tester.cpp"
 #include "class_tester.cpp"
-//#include "ent_container_test.cpp"
+#include "classContainer_tester.cpp"
+#include "enroll_tester.cpp"
+#include "lecture_tester.cpp"
+#include "feed_tester.cpp"
+#include "feedback_tester.cpp"
+#include "feedContainer_tester.cpp"
+#include "lectureContainer_tester.cpp"
+#include "enrollContainer_tester.cpp"
+#include "feedbackContainer_tester.cpp"
 
 int main(int argc, char *argv[]){
     :: testing:: InitGoogleTest(&argc, argv);
